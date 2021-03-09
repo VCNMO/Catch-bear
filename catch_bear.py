32c7d371d2f50570844319e0f13c73e6d0821d70
Makarenkomd <mahadrum@yandex.ru>
2021-03-09 13:07:15 +0300

    без начала игры

---
+++ catch_bear.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import messagebox
+
+import random
+
+def mouseclick(params):
+      y = params.y
+      x = params.x
+      s = str(x) + " " + str(y)
+      win.title(s)
+      #messagebox.showinfo(title="Поймай медведя", message=s)
+      r = 5
+      canv.create_oval(x-r,y-r,x+r,y+r,width=1)
+      
+      if abs(x - bearX) < 10 and abs(y - bearY) < 10:
+            bear=tk.PhotoImage("bear.jpg")
+            img=canv.create_image(0, 0, image=bear)            
+            #messagebox.showinfo("Поймай медведя", "поймал!")
+            
+      else:
+            s = ""
+            if y < bearY:
+                  s += " Южнее"
+            else:
+                  s += " Севернее"
+            if x > bearX:
+                  s += " Западнее"
+            else:
+                  s += " Восточнее"
+            messagebox.showinfo("Промах", s)
+       
+bearX = 150
+bearY = 70
+
+win = tk.Tk()
+win.geometry()
+canv = tk.Canvas(win, width=600, height=400, bg='green')
+canv.pack()
+
+
+win.bind("<1>",mouseclick)
+win.mainloop()
